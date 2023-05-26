6b98773c0e1db8441d41616fd242034dd683df98
Mehmet Han Ozansoy <94033888+MehmethanOzansoy@users.noreply.github.com>
2023-05-26 18:18:11 +0300

    Add files via upload

---
+++ conditions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time < 18 && time > 6)
+            {
+                System.Console.WriteLine("Greeting");
+            }
+            else
+            {
+                System.Console.WriteLine("goof evening");
+            }
+
+            bool condition = true;
+
+            if (condition)
+            {
+                Console.WriteLine("Değişken: true");
+            }
+            else
+            {
+                Console.WriteLine("Değişken: false");
+            }
+        }
+    }
+}
