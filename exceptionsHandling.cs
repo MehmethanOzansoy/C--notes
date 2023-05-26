fbe7b7215f1f0dcf3b3a2f8e6948feea25192cf6
Mehmet Han Ozansoy <94033888+MehmethanOzansoy@users.noreply.github.com>
2023-05-26 17:07:55 +0300

    Add files via upload

---
+++ exceptionsHandling.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // try
+            // {
+            //     // hata olması muhtememl yer
+            //     Console.WriteLine("Bir sayi giriniz: ");
+            //     int sayi = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+            // }
+            // catch (Exception ex)
+            // {
+            //     // hata mesajını gönderir hatayı yakalar
+
+            //     System.Console.WriteLine("Hata mesajı: " + ex.Message.ToString());
+            // }
+            // // finally
+            // // {
+            // //     // opsiyonel her koşulda çalışma sağlar
+
+            // //     Console.Write("İşlem tamamlandı");
+            // // }
+
+            // try
+            // {
+            //     int a = int.Parse(null);
+                
+            // }
+            // catch (ArgumentNullException ex)
+            // {
+                
+            //   System.Console.WriteLine("boş değer girdiniz !!!");
+            //   System.Console.WriteLine(ex);
+            // }
+
+            
+            try
+            {
+
+                int a = int.Parse("11");
+                
+            }
+            // catch (ArgumentNullException ex)
+            // {
+                
+            //   System.Console.WriteLine("boş değer girdiniz !!!");
+            //   System.Console.WriteLine(ex);
+            // }
+            catch (FormatException ex) {
+                System.Console.WriteLine("Veri tipi uygun değil");
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
+
