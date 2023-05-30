bbaaaf57f7d463b55b292833001fb45d9f3c0d56
Mehmet Han Ozansoy <94033888+MehmethanOzansoy@users.noreply.github.com>
2023-05-30 10:39:04 +0300

    Add files via upload

--- math-methods.cs
+++ math-methods.cs
@@ -1,4 +1,18 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Globalization;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -61,6 +75,44 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
 
 
+        }
+    }
+
+
+    class Result
+    {
+
+        /*
+         * Complete the 'timeConversion' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static string timeConversion(string s)
+        {
+            
+            var dt = DateTime.ParseExact(s,"hh:mm:ssttt",CultureInfo.InvariantCulture);
+
+            return $"{dt:HH:mm:ss}";
+        }
+
+    }
+
+    class Solution2
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = Result.timeConversion(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
         }
     }
 }
